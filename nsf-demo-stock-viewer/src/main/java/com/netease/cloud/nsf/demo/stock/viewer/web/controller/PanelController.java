20ecffe853515c1598ba36e97a19181584832e35
hzzhangliang5 <hzzhangliang5@corp.netease.com>
2019-04-30 12:21:54 +0800

    "I am good! chanaged in eclipse "

--- nsf-demo-stock-viewer/src/main/java/com/netease/cloud/nsf/demo/stock/viewer/web/controller/PanelController.java
+++ nsf-demo-stock-viewer/src/main/java/com/netease/cloud/nsf/demo/stock/viewer/web/controller/PanelController.java
@@ -116,7 +116,7 @@ public class PanelController {
     @GetMapping("/health")
     @ResponseBody
     public String health() {
-    	return "I am good!";
+    	return "I am good! chanaged in eclipse ";
     }
     
     @RequestMapping("/deepInvoke")
